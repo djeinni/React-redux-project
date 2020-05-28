53e72c146e275b5f8a9839c3566fc0960c0dce8a
Djeinni <ami-ba@hotmail.fr>
2020-05-28 16:00:06 +0200

    Deuxieme commit

--- src/App.js
+++ src/App.js
@@ -2,12 +2,36 @@ import React from 'react';
 import logo from './logo.svg';
 import { Counter } from './features/counter/Counter';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCoffee } from '@fortawesome/react-fontawesome'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="#">        <img src={logo} className="App-logo" alt="logo" />
+</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#"><FontAwesomeIcon icon="coffee" />
+</a>
+      </li>
+      <li class="nav-item active">
+        <a class="nav-link" href="#">panier <span class="sr-only">(current)</span></a>
+      </li>
+    </ul>
+  </div>
+</nav>
         <Counter />
         <p>
           Edit <code>src/App.js</code> and save to reload.
